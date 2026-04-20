8e345f2cf8724fa9aedd7e9f83835e8b0e10f1cc
Samuel Rey <samuelreyparaps4@gmail.com>
2026-04-19 22:29:58 -0500

    Añadir dependencia para grafica de barras

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation(libs.maps.compose)
     implementation("com.google.android.gms:play-services-maps:20.0.0")
+    implementation(libs.charts)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.credentials)
