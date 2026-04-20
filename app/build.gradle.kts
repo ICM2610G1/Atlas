5ec119f67f5afdec0366c4351f9fe1d5256e8db8
Eileen Rodriguez <brillithgualtero@gmail.com>
2026-04-20 15:49:45 -0500

    agregar dependencias de osmdroid y osmbonuspack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
     implementation(libs.firebase.database)
+    implementation("org.osmdroid:osmdroid-android:6.1.17")
+    implementation("com.github.MKergall:osmbonuspack:6.9.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
